330b1e818308680e6859df5ae00a6c9127e46ed0
ghinsghar <ghinsghargarcia@gmail.com>
2025-09-20 16:53:27 +0800

    App Commit

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+
+import CommentSection from './CommentSection';
+import ChatBubble from './ChatBubble';
+
 
 export default function App() {
+  const [bgColor, setBgColor] = useState('white');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container, { backgroundColor: bgColor }]}>
+     
+      <CommentSection/>
+      <ChatBubble/>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
 });
